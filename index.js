6bee8876589c72c9d0331c1d1838b45d09e86bab
chaitanya rishi <chaitanya167@gmail.com>
2020-08-08 20:58:24 -0400

    server and some basic html setup

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-const axios = require('axios');
-const hostname = '127.0.0.1';
+const ejs = require('ejs');
+var bodyParser = require('body-parser');
+const $ = require('jquery')
 
-const port = 3000;
+app.use(bodyParser.json());
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.set('view engine', "ejs");
+app.post('/room-req', (req, res) => {
+    const room_name = req.body.room_name;
+    // res.sendStatus(200)
+    res.render('room_created', {
+        room_name: room_name
+    })
 })
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+console.log(process.env.port)
 app.use('/', router);
 app.listen(process.env.port || 3000);
 console.log('Running at port 3000')
