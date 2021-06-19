290b3f1fc7c40084c9a23f54fa97de0d4d18c974
chaitanya rishi <chaitanya167@gmail.com>
2021-06-19 17:24:35 -0400

    update uuid package to v4

--- routes/groups.js
+++ routes/groups.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require("uuidv4");
 
 /**
  * /group:
- *   post: 
+ *   post:
  *     description: This endpoint create a new group
  *     request: -
  *     responses:
  *      str: groupId
  */
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    console.log("ok");
     // generate a random group Id
     const groupId = uuidv4();
     //create a new group with the genrated Id
-    await res.locals.client.put({
-        TableName: 'groups',
-        Item: {
-            groupId: groupId
-        }
-    }).promise();
+    await res.locals.client
+        .put({
+            TableName: "groups",
+            Item: {
+                groupId: groupId,
+            },
+        })
+        .promise();
     res.json(groupId);
     res.end();
 });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require('dotenv').config({ path: __dirname + '/.env' });
-const AWS = require('aws-sdk');
-const express = require('express');
-const groups = require('./routes/groups');
+require("dotenv").config({ path: __dirname + "/.env" });
+const AWS = require("aws-sdk");
+const express = require("express");
+const groups = require("./routes/groups");
 
 AWS.config.update({
-    region: process.env.REGION || 'us-east-1',
+    region: process.env.REGION || "us-east-1",
     accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY
+    secretAccessKey: process.env.SECRET_KEY,
 });
 
 const app = express();
@@ -15,13 +15,17 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     const client = new AWS.DynamoDB.DocumentClient({
-        region: process.env.REGION || 'us-east-1'
+        region: process.env.REGION || "us-east-1",
     });
     res.locals.client = client;
     next();
 });
 
-app.use('/groups', groups);
+app.use("/groups", groups);
+
+app.get("/", (req, res) => {
+    return res.send("ok");
+});
 
 const port = process.env.PORT || 8001;
 app.listen(port, () => console.log(`Listening on ${port}!`));
